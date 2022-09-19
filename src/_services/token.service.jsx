083b1066c409e48c5ff3b4e2967b1cc27582b78f
shreekrishnaacharya <sksharma72000@gmail.com>
2022-09-19 13:58:20 +0545

    multi login admin and user on same page

--- src/_services/token.service.jsx
+++ src/_services/token.service.jsx
@@ -6,24 +6,24 @@ class TokenService {
     }
     static interval;
     getLocalRefreshToken() {
-        const user = JSON.parse(localStorage.getItem("userModel"));
+        const user = JSON.parse(localStorage.getItem("userAdminModel"));
         return user?.refreshToken;
     }
     getLocalAccessToken() {
-        const user = JSON.parse(localStorage.getItem("userModel"));
+        const user = JSON.parse(localStorage.getItem("userAdminModel"));
         return user?.token;
     }
     updateLocalAccessToken(token) {
-        let user = JSON.parse(localStorage.getItem("userModel"));
+        let user = JSON.parse(localStorage.getItem("userAdminModel"));
         user.token = token;
-        localStorage.setItem("userModel", JSON.stringify(user));
+        localStorage.setItem("userAdminModel", JSON.stringify(user));
     }
     getUser() {
-        return JSON.parse(localStorage.getItem("userModel"));
+        return JSON.parse(localStorage.getItem("userAdminModel"));
     }
     setUser(user) {
         user["isLogin"] = true;
-        localStorage.setItem("userModel", JSON.stringify(user));
+        localStorage.setItem("userAdminModel", JSON.stringify(user));
     }
     removeUser() {
         this.store.dispatch(setLogout());
